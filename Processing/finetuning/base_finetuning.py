7241a7966d613cb3131aedec6e574d3c601a0e67
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-08-11 15:22:01 +0530

    Updated load method

--- Processing/finetuning/base_finetuning.py
+++ Processing/finetuning/base_finetuning.py
@@ -1,13 +1,43 @@
+import logging
+import os
 from abc import abstractmethod, ABC
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+LOCAL_MODEL_PATH = "QWEN2/qwen2.5-1.5B-instruct"
 
 class BaseFineTune(ABC):
     def __int__(self,
-                model_name: str):
+                model_name: str,
+                logger = None,
+                *args, **kwargs):
         self.model_name = model_name
+        self.tokenizer = None
+        self.model = None
+        self.logger = logger if logger else logging.getLogger(__name__)
 
-    @abstractmethod
+        if not os.path.exists(local_model_path):
+            self.load()
+
     def load(self):
-        pass
+        """
+        Load the ADFA Qwen2 model and tokenizer.
+        """
+        try:
+            self.logger.info("Loading ADFA Qwen2 model...")
+            self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_name,
+                torch_dtype="auto",
+                device_map="auto"
+            )
+            self.logger.info("Model and tokenizer loaded successfully.")
+            self.logger.info(f"Saving model and tokenizer to: {self.local_model_path}")
+            self.model.save_pretrained(LOCAL_MODEL_PATH)
+            self.tokenizer.save_pretrained(LOCAL_MODEL_PATH)
+            self.logger.info("✅ Model saved successfully to your local machine!")
+        except Exception as e:
+            self.logger.error(f"Error loading model {self.model_name}: {e}")
+            raise e
 
     @abstractmethod
     def finetune(self):
